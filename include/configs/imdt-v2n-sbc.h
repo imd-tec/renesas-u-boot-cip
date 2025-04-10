44792085bf8c5254b48271911938189e46f68f9d
Lewis Purvis <77275854+LSPgh@users.noreply.github.com>
2025-04-10 16:56:03 +0100

    Add rz feature support to V2N (#16)
    Update u-boot to support rz-features in V2N

--- include/configs/imdt-v2n-sbc.h
+++ include/configs/imdt-v2n-sbc.h
@@ -3,4 +3,18 @@
 
 #include "imdt-v2.h"
 
+#if defined(CONFIG_RZ_FEATURES_OPENCVA)
+    #define OCA_FEATURE \
+ 	"ocaaddr=0xA8000000 \0"     \
+ 	"ocabin=OpenCV_Bin.bin \0"  
+ 	#define LOAD_COMMAND_OPENCVA "ext4load mmc ${boot_device}:${mmcpart} ${ocaaddr} boot/${ocabin}; "
 #endif
+
+#if defined(CONFIG_RZ_FEATURES_CODECS)
+    #define CODECS_FEATURE \
+        "codaddr=0xAFD00000 \0"     \
+        "codbin=Codec_Bin.bin \0"
+    #define LOAD_COMMAND_CODEC "ext4load mmc ${boot_device}:${mmcpart} ${codaddr} boot/${codbin}; "
+#endif
+
+#endif /* __IMDT_V2N_H */

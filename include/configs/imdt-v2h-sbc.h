deb48481963cb9623af13f1f8f540fd1801d8a16
Lewis Purvis <77275854+LSPgh@users.noreply.github.com>
2026-03-26 12:27:39 +0000

    (imdt-v2h-sbc) Change V2H Feature addresses to match 6.1 device tree. Add disabling audit to bootargs. (#20)

--- include/configs/imdt-v2h-sbc.h
+++ include/configs/imdt-v2h-sbc.h
@@ -5,14 +5,14 @@
 
 #if defined(CONFIG_RZ_FEATURES_OPENCVA)
 	#define OCA_FEATURE \
- 	"ocaaddr=0xC0000000 \0"     \
+ 	"ocaaddr=0xA8000000 \0"     \
  	"ocabin=OpenCV_Bin.bin \0"  
  	#define LOAD_COMMAND_OPENCVA "ext4load mmc ${boot_device}:${mmcpart} ${ocaaddr} boot/${ocabin}; "
 #endif
 
 #if defined(CONFIG_RZ_FEATURES_CODECS)
 	#define CODECS_FEATURE \
-		"codaddr=0xC7D00000 \0"     \
+		"codaddr=0xAFD00000 \0"     \
 		"codbin=Codec_Bin.bin \0"
 	#define LOAD_COMMAND_CODEC "ext4load mmc ${boot_device}:${mmcpart} ${codaddr} boot/${codbin}; "
 #endif
